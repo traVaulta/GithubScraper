b5936bc5e2845d5f616ef89fd5120f35f1fdc8e7
Matija Čvrk <matijacvrk@gmail.com>
2020-04-02 22:22:56 +0200

    [setup] clean-up

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe(`${App.name}`, function () {
+    test('renders learn react link', () => {
+        const {getByText} = render(<App/>);
+        const linkElement = getByText(/github scraper/i);
+        expect(linkElement).toBeInTheDocument();
+    });
 });
