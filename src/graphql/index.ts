815637c27117bb7ed7364020ce18f63bdf132489
Matija Čvrk <matijacvrk@gmail.com>
2020-04-10 18:01:21 +0200

    [graphql] cache and matcher - added fragment macher and caching to github apollo client instance

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,4 +1,10 @@
-import ApolloClient, {PresetConfig} from 'apollo-boost';
+import ApolloClient, {InMemoryCache, IntrospectionFragmentMatcher, PresetConfig} from 'apollo-boost';
+import introspectionResult from './types';
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+    introspectionQueryResultData: introspectionResult
+});
+const cache = new InMemoryCache({fragmentMatcher});
 
 export const token = process.env.REACT_APP_GITHUB_TOKEN;
 export const client = new ApolloClient({
@@ -6,5 +12,6 @@ export const client = new ApolloClient({
     headers: {
         'Authorization': `bearer ${token}`,
         'Access-Control-Allow-Origin': '*'
-    }
+    },
+    cache
 } as PresetConfig);
