1ee937f15b3782bc8793314c255489481c660fcd
Matija Čvrk <matijacvrk@gmail.com>
2020-04-10 18:00:19 +0200

    [graphql] included type names in fragments

--- src/graphql/github/viewRepositorySummaryFragment.ts
+++ src/graphql/github/viewRepositorySummaryFragment.ts
@@ -2,6 +2,7 @@ import {gql} from 'apollo-boost';
 
 export const REPOSITORY_SUMMARY_FRAGMENT = gql`
 fragment RepositorySummary on Repository {
+  __typename
   name
   description
   url
--- src/graphql/github/viewUserProfileFragment.ts
+++ src/graphql/github/viewUserProfileFragment.ts
@@ -2,6 +2,7 @@ import {gql} from 'apollo-boost';
 
 export const USER_PROFILE_FRAGMENT = gql`
 fragment UserProfile on User {
+  __typename
   avatarUrl
   login
   email
