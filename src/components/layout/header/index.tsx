f986a90a71bb1e4e1852c5df3be41187fd850842
Matija Čvrk <matijacvrk@gmail.com>
2020-06-04 16:09:35 +0200

    [docs] displaying updated screenshots in readme file

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import SponsorImg from '../../../assets/powered-by.png';
 import LogoImg from '../../../assets/app-logo.png';
 
 export const AppHeader = () => (
     <header className="app-header app-header--outline">
         <div className="app-header app-header--sponsor">
             <img src={LogoImg} alt="Logo"/>
-            <img src={SponsorImg} alt="powered-by-rimac"/>
         </div>
         <div>Github Scraper</div>
     </header>
